ec054a33b0cf43703c9045e3a9aa45156d5790ec
dishakathane <dishakathane1995@gmail.com>
2022-06-30 11:52:13 +0530

    done

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -6,24 +6,14 @@ const validator = require("../validator/validator")
 let createIntern = async function (req, res) {
     try {
 
-
         let data = req.body;
-
         const { name, email, mobile, collegeName } = data
 
-        if (!validator.isValidRequestBody(data)) {     //nothing in body
-            return res.status(400).send({ status: false, message: "Please provide data in intern body..." })
-        }
+        if (!validator.isValidRequestBody(data)) { return res.status(400).send({ status: false, message: "Please provide data in intern body..." }) } //nothing in body
 
+        if (!validator.valid(name)) { return res.status(400).send({ status: false, message: "Please provide intern name..." }) } //0 length or not null or undefined
 
-        if (!validator.valid(name)) {           //0 length or not null or undefined
-            return res.status(400).send({ status: false, message: "Please provide intern name..." })
-        }
-        if (!validator.regexIntern(name)) {     //valid format of name check
-            return res.status(400).send({ status: false, message: "Please enter the valid intern name..." })
-        }
-
-
+        if (!validator.regexIntern(name)) { return res.status(400).send({ status: false, message: "Please enter the valid intern name..." }) } //valid format of name check
 
         if (!validator.valid(email)) {
             return res.status(400).send({ status: false, message: "Please enter the email..." })
@@ -32,39 +22,28 @@ let createIntern = async function (req, res) {
             return res.status(400).send({ status: false, message: "Please enter the valid email..." })
         }
         let checkEmail = await internModel.findOne({ email: email })
-        if (checkEmail) {
-            return res.status(400).send({ status: false, message: "email already exist..." })
-        }
+        if (checkEmail) { return res.status(400).send({ status: false, message: "email already exist..." }) }
 
+        if (!validator.valid(mobile)) { return res.status(400).send({ status: false, message: "Please enter the mobile number... " }); }
+        if (!validator.moblieRegex(mobile)) { return res.status(400).send({ status: false, message: "please enter the valid mobile number..." }) }
 
-        if (!validator.valid(mobile)) {
-            return res.status(400).send({ status: false, message: "Please enter the mobile number... " });
-        }
-        if (!validator.moblieRegex(mobile)) {
-            return res.status(400).send({ status: false, message: "please enter the valid mobile number..." })
-        }
         let checkMobile = await internModel.findOne({ mobile: mobile })
-        if (checkMobile) {
-            return res.status(400).send({ status: false, message: "mobile number is already present..." })
-        }
+        if (checkMobile) { return res.status(400).send({ status: false, message: "mobile number is already present..." }) }
 
-        if (!validator.valid(collegeName)) {
-            return res.status(400).send({ status: false, message: "enter the college name..." });
-        }
+        if (!validator.valid(collegeName)) { return res.status(400).send({ status: false, message: "enter the college name..." }) }
+
         let collegeDetails = await collegeModel.findOne({ name: collegeName })
 
-        if (!collegeDetails) {
-            return res.status(404).send({ status: false, message: "college does not exist..." })
-        }
-        collegeId = collegeDetails._id;
+        if (!collegeDetails) { return res.status(404).send({ status: false, message: "college does not exist..." }) }
+
+        let collegeId = collegeDetails._id;
 
         const data1 = { name, email, mobile, collegeId }
 
         let internData = await internModel.create(data1)
-        res.status(201).send({ status: false, data: internData })
-    } catch (err) {
-        return res.status(500).send({ status: false, message: err.message })
-    }
+        return res.status(201).send({ status: true, data: internData })
+
+    } catch (err) { return res.status(500).send({ status: false, message: err.message }) }
 }
 
 
@@ -74,15 +53,13 @@ let getInternByCollege = async function (req, res) {
         let collegeName = req.query.collegeName;
 
         if (!validator.valid(collegeName) || !collegeName || !validator.isREgexName(collegeName)) {
-
             return res.status(400).send({ status: false, message: "Please enter college shortname with correct format..." })
         }
 
         const dataTobePresented = await collegeModel.findOne({ name: collegeName })
 
-        if (!dataTobePresented) {
-            return res.status(404).send({ status: false, message: "this college not found..." })
-        }
+        if (!dataTobePresented) { return res.status(404).send({ status: false, message: "this college not found..." }) }
+
         let collegeId = dataTobePresented._id
 
         let intern = await internModel.find({ collegeId: collegeId }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
@@ -99,7 +76,7 @@ let getInternByCollege = async function (req, res) {
             })
         }
 
-        res.status(200).send({
+        return res.status(200).send({
             status: true,
             data: {
                 name: dataTobePresented.name,
@@ -109,9 +86,7 @@ let getInternByCollege = async function (req, res) {
             }
         })
 
-    } catch (err) {
-        return res.status(500).send({ status: false, message: err.message })
-    }
+    } catch (err) {  return res.status(500).send({ status: false, message: err.message })  }
 }
 
 module.exports = { createIntern, getInternByCollege };
